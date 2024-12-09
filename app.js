33dd8172b4f32696d8397f531521d7e21a5ceb8d
Marius Daniel Vasilcanu <74616980+MariusVasilcanu28@users.noreply.github.com>
2024-12-09 22:34:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ app.use(
   }).single("image")
 );
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 app.use(
   "/public/multedImg",
@@ -157,7 +157,8 @@ app.use((error, req, res, next) => {
 });
 
 // MongoDB Connection
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error(err));
 
